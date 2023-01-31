d518ac7b968f0eec95a5e92a926c2ab66002894a
Rayli-da <lidahua@graduate.utm.my>
2023-01-31 21:52:08 +0800

    delete

--- s.py
+++
@@ -1,7 +0,0 @@
-first_int= 10
-second_int= 20
-if first_int>second_int:
-
-    print ('first is bigger')
-else:
-    print('second is bigger')    
--- titanic.py
+++
@@ -1,9 +0,0 @@
-
-
-import csv
-
-file=open('TitanicSurvival.csv','r')
-data=list(csv.reader(file,delimiter = "."))
-file.close()
-
-print(data)
