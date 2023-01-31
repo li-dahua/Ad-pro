1be91220b5a6a3f527b3cb736acf2846d9e3c6c6
Rayli-da <lidahua@graduate.utm.my>
2023-01-31 14:17:02 +0800

    array

---
+++ array/# Indexing and slicing.py
@@ -0,0 +1,7 @@
+# Indexing and slicing
+import numpy as np
+grades = np.array([[87,96,70],[100,87,90],[94,77,90],[100, 81,82]])
+grades_1 = grades[0,1]
+grades_2 = grades[1]
+grades_3 = grades[[1,3]]
+print( grades_3)
---
+++ array/attribute.py
@@ -0,0 +1,10 @@
+import numpy as np 
+
+integers = np.array([[3,2,1],[6,7,8]])
+floats = np.array([0.3,0.2,0.9,0.44,0.11,0.08])
+
+integers_shape = integers.shape 
+floats_shape = floats.shape 
+
+print(integers_shape)
+print(floats_shape)
